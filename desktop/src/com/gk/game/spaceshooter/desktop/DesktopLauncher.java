4e366057674d61e5c46159fde0237ef63ff8d9ac
Gregory Kiselman <gregory.kiselman@gmail.com>
2020-05-24 21:56:03 +0300

    Initial commit

---
+++ desktop/src/com/gk/game/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gk.game.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gk.game.spaceshooter.SpaceShooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooterGame(), config);
+	}
+}
